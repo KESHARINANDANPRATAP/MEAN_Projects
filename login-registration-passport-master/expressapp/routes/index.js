67f5276cb767c2200e49af83519a31c56790f149
KESHARI NANDAN PRATAP <64979238+KESHARINANDANPRATAP@users.noreply.github.com>
2021-01-11 13:47:45 +0530

    Add files via upload

---
+++ login-registration-passport-master/expressapp/routes/index.js
@@ -0,0 +1,11 @@
+// Submited By-KESHARI NANDAN PRATAP
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+// Submited By-KESHARI NANDAN PRATAP
